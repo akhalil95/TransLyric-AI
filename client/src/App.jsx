2d94bb84f9efeebe2b70a4e7c13068a5302f0c4c
Ayesha <68371207+akhalil95@users.noreply.github.com>
2025-08-11 15:00:21 -0600

    initial client set up

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoPlayer from "./components/VideoPlayer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Music Video Translator</h1>
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoPlayer = () => {
+  return <div>Video Player Placeholder</div>;
+};
+
+export default VideoPlayer;
